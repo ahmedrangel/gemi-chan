a374441f959d3a8f7e5f3b23847e56649ea20923
Ahmed Rangel <50090595+ahmedrangel@users.noreply.github.com>
2023-06-04 17:04:57 -0500

    Update database.js

--- server/api/database.js
+++ server/api/database.js
@@ -1,6 +1,9 @@
 export default defineEventHandler((event) => {
     // the type `KVNamespace` comes from the @cloudflare/workers-types package
     const ImgurDiscord = event.context.cloudflare.env.ImgurDiscord;
-  
-    return ImgurDiscord;
+    (async() => {
+    const select = await ImgurDiscord.prepare(`SELECT * FROM imgur_discord`);
+    const select_data = await select.all();
+    return select_data;
+    });
   });
