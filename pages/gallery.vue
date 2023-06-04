f16f680c390087b6aaf2ad2104611a6c390359ff
Ahmed Rangel <50090595+ahmedrangel@users.noreply.github.com>
2023-06-04 17:30:12 -0500

    Update gallery.vue

--- pages/gallery.vue
+++ pages/gallery.vue
@@ -1,7 +1,7 @@
 <script setup>
 const { data: results } = await useFetch("/api/database");
 (async() => {
-  console.log(results.value);
+  console.log(results.value[0]);
     const json_fetch = await fetch(`https://dev.ahmedrangel.com/imgur/me/gallery`);
     const json = await json_fetch.json();
     json.forEach((data) => {
