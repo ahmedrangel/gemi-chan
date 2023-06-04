b2c676d5757f1dded09aa520fe1d257b494e7875
Ahmed Rangel <50090595+ahmedrangel@users.noreply.github.com>
2023-06-04 17:16:23 -0500

    Update database.js

--- server/api/database.js
+++ server/api/database.js
@@ -1,8 +1,6 @@
-export default defineEventHandler((event) => {
+export default defineEventHandler(async(event) => {
     const ImgurDiscord = event.context.cloudflare.env.ImgurDiscord;
-    (async() => {
     const select = await ImgurDiscord.prepare(`SELECT * FROM imgur_discord`);
     const select_data = await select.all();
     return select_data;
-    });
   });
