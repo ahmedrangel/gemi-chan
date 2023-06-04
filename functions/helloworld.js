f220a8baecfc5951fdfda41ebcbccab7204bf8c3
Ahmed Rangel <50090595+ahmedrangel@users.noreply.github.com>
2023-06-04 16:24:27 -0500

    Delete helloworld.js

--- functions/helloworld.js
+++
@@ -1,5 +0,0 @@
-export async function onRequest(context) {
-    const ps = context.env.ImgurDiscord.prepare('SELECT * imgur_discord');
-    const data = await ps.first(); 
-    return Response.json(data);
-  }
