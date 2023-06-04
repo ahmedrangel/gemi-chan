7c6fd696901fb8baad23d95dcb62f839318f837d
Ahmed Rangel <50090595+ahmedrangel@users.noreply.github.com>
2023-06-04 16:39:28 -0500

    Create database.js

---
+++ server/api/database.js
@@ -0,0 +1,8 @@
+export default defineEventHandler((event) => {
+    // the type `KVNamespace` comes from the @cloudflare/workers-types package
+    const ImgurDiscord = event.context.cloudflare.env.ImgurDiscord;
+  
+    return {
+      // ...
+    };
+  });
