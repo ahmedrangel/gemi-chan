ba94d72bdcd802bb4feb3affceda90c6c0d21e05
Ahmed Rangel <50090595+ahmedrangel@users.noreply.github.com>
2023-06-11 15:39:49 -0500

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,16 @@
 export default ({     
   css: [
-      "bootstrap/dist/css/bootstrap.min.css",
-      "~/assets/css/gallery.css",
-    ],
+    "bootstrap/dist/css/bootstrap.min.css",
+    "~/assets/css/gallery.css",
+  ],
   
   js: [
     "bootstrap/dist/js/bootstrap.bundle.min.js"
   ],
-    
+
   app: {
     head: {
       script: [{src: "https://cdnjs.cloudflare.com/ajax/libs/iconify/3.1.0/iconify.min.js"}]
     }
   }
-  })
+})
