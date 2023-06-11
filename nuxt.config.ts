2d6296ea993c5a50e85fa8f8d39e9f8877b0fabf
Ahmed Rangel <50090595+ahmedrangel@users.noreply.github.com>
2023-06-11 06:11:07 -0500

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,6 @@ export default ({
   css: [
       "bootstrap/dist/css/bootstrap.min.css",
       "~/assets/css/gallery.css",
-    ]
+    ],
+  script: [{src: "https://kit.fontawesome.com/cc91b92ca8.js"}]
   })
