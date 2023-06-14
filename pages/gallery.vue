a7f32b61109e5a2188fd7233a826f74e71ce82a4
Ahmed Rangel <50090595+ahmedrangel@users.noreply.github.com>
2023-06-14 00:11:52 -0500

    Update gallery.vue

--- pages/gallery.vue
+++ pages/gallery.vue
@@ -140,8 +140,9 @@ const getDateFromTimeStamp = (timestamp) => {
                       <span class="iconify" data-icon="ph:terminal-window-duotone" data-inline="false"></span>
                     /generar
                     </span>&nbsp;
-                    <span class="badge fw-normal"><span class="iconify" data-icon="ph:cube-duotone" data-inline="false"></span>
-                      /variar
+                    <span class="badge fw-normal">
+                      <span class="iconify" data-icon="ph:cube-duotone" data-inline="false"></span>
+                    /variar
                     </span>
                   </h5>
               </div>
