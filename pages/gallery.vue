d0500be7fd10cd8dc9bebdcc3b5d4abf373e48f1
Ahmed Rangel <50090595+ahmedrangel@users.noreply.github.com>
2023-06-13 23:40:50 -0500

    Update gallery.vue

--- pages/gallery.vue
+++ pages/gallery.vue
@@ -133,10 +133,10 @@ const getDateFromTimeStamp = (timestamp) => {
       <div class="container">
           <div class="row justify-content-center">
               <div class="col-12">
-                  <h2 class="mb-3 fw-bold">Galería de imágenes generadas por IA DALL-E (OpenAI)</h2>
-                  <h5 class="fw-light">Obtenido a través de peticiones de usuarios al interactuar con el bot de Discord Gemi-chan</h5>
-                  <h5 class="fw-light">Utilizando los comandos: 
-                    <span class="badge bg-secondary fw-normal">/generar</span> <span class="badge bg-secondary fw-normal">/variar</span>
+                  <h2 id="titulo" class="mb-3 fw-bold">Galería de imágenes generadas por IA DALL-E (OpenAI)</h2>
+                  <h5 id="descripcion" class="fw-light">Obtenido a través de peticiones de usuarios al interactuar con comandos con el bot de Discord Gemi-chan</h5>
+                  <h5 id="comandos" class="fw-light">Comandos: 
+                    <span class="badge bg-secondary fw-normal">/generar</span> <span class="badge bg-secondary fw-normal">/variar</span> en el chat.
                   </h5>
               </div>
               <div id="gallery" class="row pb-3"></div>
