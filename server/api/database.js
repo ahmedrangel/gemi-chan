b9b0c97ca5be7285ae61b1b697695a174d0d364f
Ahmed Rangel <50090595+ahmedrangel@users.noreply.github.com>
2023-07-01 08:14:51 -0500

    Update database.js

--- server/api/database.js
+++ server/api/database.js
@@ -1,6 +1,6 @@
 export default defineEventHandler(async() => {
-    const ImgurDiscord = process.env.ImgurDiscord;
-    const select = await ImgurDiscord.prepare(`SELECT * FROM imgur_discord ORDER BY timestamp DESC`);
-    const select_data = await select.all();
-    return select_data;
-  });
+  const ImgurDiscord = process.env.ImgurDiscord;
+  const select = await ImgurDiscord.prepare(`SELECT * FROM imgur_discord ORDER BY timestamp DESC`);
+  const select_data = await select.all();
+  return select_data;
+});
